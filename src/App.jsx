27a19c15ae797b2e66be8d5d1e76de1bd202a83c
hazel1814 <terilolots@gmail.com>
2024-08-25 20:58:01 +0530

    Navbar, body and video carousel section

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights/>
+    </main>
+  )
+}
